aba15b3e247c8a6d34c79f5cab85fa6b130f57cc
Johannes Wilm <mail@johanneswilm.org>
2019-01-26 10:38:15 +0100

    transpile, fixes #6

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  plugins: [
-    require('rollup-plugin-commonjs')(),
-    require('rollup-plugin-node-resolve')()
-  ]
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import buble from 'rollup-plugin-buble'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+    plugins: [
+        commonjs(),
+        resolve(),
+        buble({
+          transforms: { dangerousForOf: true }
+        }),
+        terser()
+    ],
+    output: {
+        format: 'cjs'
+    }
 }
