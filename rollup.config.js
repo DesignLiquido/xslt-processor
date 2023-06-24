8e4d698b9b7028bd6d0ca66c62dc86c903a9380d
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-06-24 14:57:52 -0700

    Updating libraries.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import buble from 'rollup-plugin-buble'
-import { terser } from 'rollup-plugin-terser'
+import {terser} from 'rollup-plugin-terser'
 
 export default {
     plugins: [
         commonjs(),
         resolve(),
         buble({
-          transforms: { dangerousForOf: true }
+          transforms: {dangerousForOf: true}
         }),
         terser()
     ],
