aa5f88a068d297a20a536459505afad7edac8ade
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-06-26 16:02:02 -0700

    More linter and Jest settings.

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,15 @@
 import type { Config } from '@jest/types';
-export default async (): Promise<Config.InitialOptions> => {
-	return {
-        verbose: true,
-        modulePathIgnorePatterns: ['<rootDir>/dist/'],
-        testEnvironment: 'node',
-        coverageReporters: ['json-summary', 'lcov', 'text', 'text-summary'],
-		displayName: {
-			name: 'xslt-transform',
-			color: 'greenBright'
-		},
-		detectOpenHandles: true,
-		transform: {
-			"\\.[jt]sx?$": "babel-jest"
-		}
-    };
-};
+export default async (): Promise<Config.InitialOptions> => ({
+    verbose: true,
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    testEnvironment: 'node',
+    coverageReporters: ['json-summary', 'lcov', 'text', 'text-summary'],
+    displayName: {
+        name: 'xslt-transform',
+        color: 'greenBright'
+    },
+    detectOpenHandles: true,
+    transform: {
+        '\\.[jt]sx?$': 'babel-jest'
+    }
+});
