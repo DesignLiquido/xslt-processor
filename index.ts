b8dc25a304fcb53b13c597cec0e142ba6d84e7c6
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-06-28 17:30:07 -0700

    Exporting `xmlEscapeText` to be used in interactive tests.

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { XPath } from './src/xpath';
 export { Xslt } from './src/xslt';
 export { xmlParse } from './src/dom';
+export { xmlEscapeText } from './src/dom/util';
 export { ExprContext } from './src/xpath';
