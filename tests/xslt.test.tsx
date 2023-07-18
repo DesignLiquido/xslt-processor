87554c4e39d3f82d9962c782e64b6e8f0a6bb4af
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-07-18 08:23:23 -0700

    All three tests passing.

--- tests/xslt.test.tsx
+++ tests/xslt.test.tsx
@@ -134,7 +134,7 @@ describe('xslt', () => {
         // The base match algorithm should consider them.
         // For more information: https://github.com/DesignLiquido/xslt-processor/pull/62#issuecomment-1636684453
 
-        it.only('Example 1 from Marco', () => {
+        it('Example 1 from Marco', () => {
             const xmlString = (
                 <root>
                     <typeA />
@@ -179,7 +179,7 @@ describe('xslt', () => {
             assert.equal(outXmlString, expectedOutString);
         });
 
-        it.skip('Example 2 from Marco', () => {
+        it('Example 2 from Marco', () => {
             const xmlString = (
                 <root>
                     <typeA />
@@ -222,7 +222,7 @@ describe('xslt', () => {
             assert.equal(outXmlString, expectedOutString);
         });
 
-        it.skip('Example 3 from Marco', () => {
+        it('Example 3 from Marco', () => {
             const xmlString = (
                 <root>
                     <typeA />
