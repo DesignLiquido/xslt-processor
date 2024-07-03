b4dc330a7c2bff7f3aedf4e7a17b2d64710166e9
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-07-03 12:04:34 -0700

    Fixing `<xsl:include>` test.

--- tests/xslt/include.test.tsx
+++ tests/xslt/include.test.tsx
@@ -4,12 +4,12 @@ import { XmlParser } from "../../src/dom";
 import { Xslt } from "../../src/xslt";
 
 describe('xsl:include', () => {
-    it.skip('Trivial', async () => {
+    it('Trivial', async () => {
         const xmlSource = `<html></html>`;
 
         const xsltSource = `<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:fo="http://www.w3.org/1999/XSL/Format">
             <xsl:output method="html" indent="yes"/>
-            <xsl:include href="https://raw.githubusercontent.com/DesignLiquido/xslt-processor/xsl-include/examples/head.xsl"/>
+            <xsl:include href="https://raw.githubusercontent.com/DesignLiquido/xslt-processor/main/examples/head.xsl"/>
         </xsl:stylesheet>`;
 
         const xsltClass = new Xslt();
