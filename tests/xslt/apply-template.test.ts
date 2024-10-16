a64644eae18d9c276c85aa5c4dc93cb809bb9564
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-10-15 17:34:55 -0700

    Adding unit test reported in https://github.com/DesignLiquido/xslt-processor/issues/108.

---
+++ tests/xslt/apply-template.test.ts
@@ -0,0 +1,41 @@
+import assert from 'assert';
+
+import { Xslt } from '../../src/xslt';
+import { XmlParser } from '../../src/dom';
+
+describe('xsl:apply-template', () => {
+    /**
+     * Returning: '<div><h2>test1</h2><p> hello<span>replaced text</span></p></div>'
+     * Expected is: '<div><h2>test1</h2><p>This is <span>replaced text</span> hello</p></div>'
+     */
+    it.skip('XSLT apply-template inside text test (https://github.com/DesignLiquido/xslt-processor/issues/108)', async () => {
+        const xmlString = `<root>
+          <test name="test1">This is <repl>text</repl> hello</test>
+        </root>`;
+
+        const xsltString = `<?xml version="1.0"?>
+          <xsl:stylesheet version="1.0">
+            <xsl:template match="repl">
+              <span>replaced <xsl:value-of select="." /></span>
+            </xsl:template>
+            <xsl:template match="/">
+              <div>
+                <h2><xsl:value-of select="test/@name" /></h2>
+                <p><xsl:apply-templates select="test/node()" /></p>
+              </div>
+            </xsl:template>
+          </xsl:stylesheet>`;
+
+        const expectedOutString = `<div><h2>test1</h2><p>This is <span>replaced text</span> hello</p></div>`;
+
+        const xsltClass = new Xslt();
+        const xmlParser = new XmlParser();
+        const xml = xmlParser.xmlParse(xmlString);
+        const xslt = xmlParser.xmlParse(xsltString);
+
+        const outXmlString = await xsltClass.xsltProcess(xml, xslt);
+
+        assert.equal(outXmlString, expectedOutString);
+        // assert.ok(outXmlString);
+    });
+});
