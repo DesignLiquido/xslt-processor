1892a34452a9348223e4abd7d691cba946417b10
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-10-15 17:46:24 -0700

    Just adding the namespace to test in other stricter XSLT transformers.

--- tests/xslt/apply-template.test.ts
+++ tests/xslt/apply-template.test.ts
@@ -14,7 +14,7 @@ describe('xsl:apply-template', () => {
         </root>`;
 
         const xsltString = `<?xml version="1.0"?>
-          <xsl:stylesheet version="1.0">
+          <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xs="http://www.w3.org/2001/XMLSchema" version="1.0">
             <xsl:template match="repl">
               <span>replaced <xsl:value-of select="." /></span>
             </xsl:template>
