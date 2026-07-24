3b70f3565d5aff91cdddf0fddf142c0cb13e30b6
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-07-24 10:01:23 -0700

    Moving issue-specific unit tests to other test files.

--- tests/xslt/for-each.test.tsx
+++ tests/xslt/for-each.test.tsx
@@ -206,4 +206,65 @@ describe('xsl:for-each', () => {
         // Primary sort by primary (1, 1, 2, 2), then by secondary: (1,a), (1,b), (2,a), (2,b) = B, D, C, A
         assert.equal(html, 'BDCA');
     });
+
+    it('xsl:element inside for-each uses correct context node (issue #160)', async () => {
+        const xmlString = `
+        <data>
+            <item id="1">Item 1</item>
+            <item id="2">Item 2</item>
+        </data>`;
+
+        const xsltString = `
+        <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+            <xsl:template match="/">
+                <root>
+                    <xsl:for-each select="data/item">
+                        <xsl:element name="row">
+                            <xsl:value-of select="."/>
+                        </xsl:element>
+                    </xsl:for-each>
+                </root>
+            </xsl:template>
+        </xsl:stylesheet>`;
+
+        const expectedOutString = `<root><row>Item 1</row><row>Item 2</row></root>`;
+
+        const xsltClass = new Xslt();
+        const xmlParser = new XmlParser();
+        const xml = xmlParser.xmlParse(xmlString);
+        const xslt = xmlParser.xmlParse(xsltString);
+        const outXmlString = await xsltClass.xsltProcess(xml, xslt);
+
+        assert.equal(outXmlString.replace(/\s+/g, ''), expectedOutString.replace(/\s+/g, ''));
+    });
+
+    it('mixing xsl:element and literal tags preserves order (issue #161)', async () => {
+        const xmlString = `
+        <data>
+            <item id="1">Item 1</item>
+            <item id="2">Item 2</item>
+        </data>`;
+
+        const xsltString = `
+        <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+            <xsl:template match="/">
+                <root>
+                    <xsl:for-each select="data/item">
+                        <xsl:element name="generated">Gen <xsl:value-of select="@id"/></xsl:element>
+                        <literal>Lit <xsl:value-of select="@id"/></literal>
+                    </xsl:for-each>
+                </root>
+            </xsl:template>
+        </xsl:stylesheet>`;
+
+        const expectedOutString = `<root><generated>Gen 1</generated><literal>Lit 1</literal><generated>Gen 2</generated><literal>Lit 2</literal></root>`;
+
+        const xsltClass = new Xslt();
+        const xmlParser = new XmlParser();
+        const xml = xmlParser.xmlParse(xmlString);
+        const xslt = xmlParser.xmlParse(xsltString);
+        const outXmlString = await xsltClass.xsltProcess(xml, xslt);
+
+        assert.equal(outXmlString.replace(/\s+/g, ''), expectedOutString.replace(/\s+/g, ''));
+    });
 });
--- tests/xslt/xslt.test.tsx
+++ tests/xslt/xslt.test.tsx
@@ -580,4 +580,33 @@ describe('xslt', () => {
 
             assert.equal(html, '<result>42</result>');
         });
+
+        it("document('') resolves to the stylesheet itself (issue #211)", async () => {
+            const xmlString = `<page><message/></page>`;
+
+            const xsltString = `
+            <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+                <xsl:variable name="strings" select="document('')/xsl:stylesheet/xsl:template[@name='strings']" />
+
+                <xsl:template match="/">
+                    <xsl:apply-templates select="/page/message"/>
+                </xsl:template>
+
+                <xsl:template match="/page/message">
+                    <div><xsl:value-of select="$strings/string"/></div>
+                </xsl:template>
+
+                <xsl:template name="strings">
+                    <string>Hehe</string>
+                </xsl:template>
+            </xsl:stylesheet>`;
+
+            const xsltClass = new Xslt();
+            const xmlParser = new XmlParser();
+            const xml = xmlParser.xmlParse(xmlString);
+            const xslt = xmlParser.xmlParse(xsltString);
+            const outXmlString = await xsltClass.xsltProcess(xml, xslt);
+
+            assert.equal(outXmlString.replace(/\s+/g, ''), '<div>Hehe</div>');
+        });
     });
